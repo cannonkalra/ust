ec42ba7214bb1b6e72e8d17dc96224c694f3c2f8
Cannon Kalra <cannonkalra@gmail.com>
2024-09-26 00:38:36 +0530

    [Add] basic prime code

---
+++ chapter-1/prime-0.py
@@ -0,0 +1,12 @@
+def p(n):
+    c = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            c += 1
+    if c == 2:
+        print(f"{n} is prime")
+    else:
+        print(f"{n} is not prime")
+
+
+p(24104567)
